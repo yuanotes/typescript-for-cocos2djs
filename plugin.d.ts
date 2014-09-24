e163732704125661bd0abc8ef7a14985ba1d85c3
Yuan Chen <yuanotes@gmail.com>
2014-09-24 12:42:55 +0800

    Update plugin type definiton.

--- plugin.d.ts
+++ plugin.d.ts
@@ -2,13 +2,19 @@ declare module plugin {
     interface PluginProtocol {
         configDeveloperInfo(info: Object);
         setDebugMode(debug: boolean);
+        getPluginName() : string;
+        callFuncWithParam(funcName: string, ...params) : void;
+        callStringFuncWithParam(funcName: string, ...params): string;
+        callIntFuncWithParam(funcName: string, ...params) : number;
+        callFloatFuncWithParam(funcName: string, ...params) : number;
+        callBoolFuncWithParam(funcName: string, ...params) : boolean;
     }
     interface ProtocolAds extends PluginProtocol {
         showAds(type: number, size: number, pos: number);
         hideAds(type: number);
     }
     interface ProtocolAnalytics extends PluginProtocol {
-        logEvent(name: string, obj: Object);
+        logEvent(name: string, obj?: Object);
     }
     class PluginManager {
         static getInstance(): PluginManager;
